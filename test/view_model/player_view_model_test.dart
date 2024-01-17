725c49f7cd280f7d597d482d7c67e09359f9d31a
Bruno <brunonasccimentto@gmail.com>
2024-01-16 22:24:48 -0300

    test inversion dependency

--- test/view_model/player_view_model_test.dart
+++ test/view_model/player_view_model_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fodinha_flutter/model/player/player.dart';
+import 'package:fodinha_flutter/model/player/player_repository.dart';
+import 'package:fodinha_flutter/services/database.dart';
 import 'package:fodinha_flutter/view_model/player_view_model/player_view_model.dart';
 
 
@@ -8,7 +10,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   group('Player view model test', () {
     test('deve retornar uma lista de jogadores', () {
-      final playerRepository = PlayerViewModel();
+      final playerRepository = PlayerViewModel(repository: PlayerRepository(database: DatabaseService()));
       final result = playerRepository.getPlayerList();
 
       expect(result, isA<Future<List<PlayerModel>>>());
