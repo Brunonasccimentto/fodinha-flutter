6a4acfd222634834b231873ba4fdbd22615c467d
Bruno <brunonasccimentto@gmail.com>
2024-01-18 16:07:12 -0300

    feat: add appbar property to custom widget AppScreen for custumize player_history view

--- lib/widgets/atoms/app_screen.dart
+++ lib/widgets/atoms/app_screen.dart
@@ -4,12 +4,14 @@ class AppScreen extends StatelessWidget {
   final List<Widget> widgets;
   final Color? backgroundColor;
   final MainAxisAlignment? aligment;
+  final AppBar? appbar;
 
-  const AppScreen({super.key, required this.widgets, this.backgroundColor, this.aligment});
+  const AppScreen({super.key, required this.widgets, this.backgroundColor, this.aligment, this.appbar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: appbar,
       backgroundColor: backgroundColor ?? Theme.of(context).colorScheme.background,
       body: SafeArea(
         child: Center(
